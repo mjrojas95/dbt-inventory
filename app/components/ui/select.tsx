261ec07023f23248a8eb0a357543cd31d92acc60
mjrojas95 <matthew.james.rojas@gmail.com>
2025-02-04 13:57:53 -0800

    Add UI components

---
+++ app/components/ui/select.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import * as React from "react"
+import * as SelectPrimitive from "@radix-ui/react-select"
+
+const Select = SelectPrimitive.Root
+const SelectGroup = SelectPrimitive.Group
+const SelectValue = SelectPrimitive.Value
+const SelectTrigger = SelectPrimitive.Trigger
+const SelectContent = SelectPrimitive.Content
+const SelectItem = SelectPrimitive.Item
+
+export { Select, SelectGroup, SelectValue, SelectTrigger, SelectContent, SelectItem }
